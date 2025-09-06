a2444da27ec5d2a98736540ee89916546bbc70b6
lalitkishor <lalitkishork73@gmail.com>
2025-09-06 13:20:38 +0530

    Refactor Project component: improve code formatting, enhance marquee functionality, and update project data structure for better readability.

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -11,7 +11,7 @@ export default function Home () {
       <Hero />
       <Projects />
       <About />
-      <Clients />
+      <Clients /> 
       <Showreel />
       <LatestInsights/>
     </div>
